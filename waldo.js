38920e8716d9c189d12b411423881df2911b5893
camueljakcson <your@email.com>
2018-06-07 15:55:34 +0000

    Using callback functions to print index of Waldo when found in array

---
+++ waldo.js
@@ -0,0 +1,14 @@
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found(i);
+    }
+  }
+}
+
+function actionWhenFound(waldoIndex) {
+
+  console.log("Found Waldo at index " + waldoIndex);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
