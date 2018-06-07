29d3b258aa8efc5030305e0ca34ad977f4f413f8
camueljakcson <your@email.com>
2018-06-07 23:56:34 +0000

    using closures; decresing counter and loggin different message

---
+++ counter.js
@@ -0,0 +1,32 @@
+var countdownGenerator = function (x) {
+
+  var counter = x;
+
+  return function() {
+
+    if (counter >= 1 ) {
+      console.log(`T-minus ${counter}`);
+      counter--
+
+    } else if (counter === 0) {
+      console.log(`Blast off!`);
+      counter--
+
+    } else {
+      console.log(`Rockets already gone, bub!`);
+      counter--
+    }
+
+    return counter
+  };
+
+}
+
+
+var countdown = countdownGenerator(3);
+
+countdown()
+countdown()
+countdown()
+countdown()
+countdown()
