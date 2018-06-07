4c61bcefaede5e7beb4393dc18eb9c308b650824
camueljakcson <your@email.com>
2018-06-07 16:09:30 +0000

    added second function using forEach to loop through array

--- waldo.js
+++ waldo.js
@@ -1,9 +1,35 @@
+//
+
 function findWaldo(arr, found) {
+
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
       found(i);
     }
   }
+}
+
+function actionWhenFound(waldoIndex) {
+
+  console.log("Found Waldo at index " + waldoIndex);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+// **************************************************************************
+
+
+function findWaldo(arr, found) {
+
+arr.forEach(function (name, index) {
+  if (name === "Waldo") {
+    found(index)
+    //found(name)
+  }
+});
+
 }
 
 function actionWhenFound(waldoIndex) {
